586b0302567fc4606e82edcb9812f036b499428c
John Carmichael <john.carmichael@liffery.com>
2021-08-10 12:16:38 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <lorem-ipsum/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import LoremIpsum from "./components/LoremIpsum";
+
+export default {
+  name: 'App',
+  components: {
+    LoremIpsum,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body{
+  padding-top: env(safe-area-inset-top);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
